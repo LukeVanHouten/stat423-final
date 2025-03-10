500065ce7017ed5d79994299d94d2a050bb64271
LukeVanHouten <lukevh@uw.edu>
2025-03-10 11:48:19 -0700

    glm final

--- glm.R
+++ glm.R
@@ -1,45 +1,87 @@
 library(tidyverse)
 library(glmnet)
 
-set.seed(123)
-
 data <- read.csv("heart_failure_clinical_records_dataset.csv")
 
-R <- 10000
-boot_index_10000 <- sample(nrow(data), R, replace=TRUE)
-boot_heart_10000 <- data[boot_index_10000, ]
-boot_heart_10000$n <- 1:R
+# Untouched data, no seed set
+accs <- numeric(20)
+for (i in 1:20) {
+    data$n <- 1:nrow(data)
+    train_basic <- data %>% 
+        sample_frac(0.8)
+    test_basic <- anti_join(data, train_basic, by = "n") %>%
+        select(-n)
+    train_basic <- select(train_basic, -n)
+    x_test_basic <- select(test_basic, -DEATH_EVENT)
+    y_test_basic <- test_basic$DEATH_EVENT
+    
+    logistic_model_i <- glm(DEATH_EVENT ~ ., data = train_basic, 
+                            family = "binomial")
+    summary(logistic_model_i)
+    
+    predictions_i <- predict(logistic_model_i, newdata = x_test_basic, 
+                           type = "response") %>%
+        as.data.frame() %>%
+        `colnames<-`(c("death_prob")) %>%
+        mutate(pred = as.numeric(death_prob > 0.5)) %>%
+        cbind(., y_test_basic)
+    
+    ct_basic <- table(predictions_i$pred, predictions_i$y_test_basic)
+    
+    accuracy_basic <- sum(diag(ct_basic)) / sum(ct_basic)
+    accs[i] <- accuracy_basic
+}
 
-boot_data <- boot_heart_10000 %>%
-    mutate(log_age = log(age), log_serum_creatinine = log(serum_creatinine),
-           log_creatinine_phosphokinase = log(creatinine_phosphokinase),
-           anaemia = as.factor(anaemia), diabetes = as.factor(diabetes), 
-           high_blood_pressure = as.factor(high_blood_pressure), 
-           sex = as.factor(sex), smoking = as.factor(smoking)) %>%
-    select(-age, -serum_creatinine, -creatinine_phosphokinase) %>%
-    mutate(n = 1:n())
+accs
+mean(accs)
+sd(accs)
+
+transformed_df <- read.csv("death_transformed.csv") 
+
+boot_data <- transformed_df %>%
+    select(-time) %>%
+    mutate(anaemia = as.factor(anaemia), diabetes = as.factor(diabetes),
+           high_blood_pressure = as.factor(high_blood_pressure),
+           sex = as.factor(sex), smoking = as.factor(smoking))
+
+set.seed(123)
 
 train <- boot_data %>% 
     sample_frac(0.8)
-test <- anti_join(boot_data, train, by = "n") %>%
-    select(-n)
-train <- select(train, -n)
+test <- anti_join(boot_data, train, by = "X") %>%
+    select(-X)
+train <- select(train, -X)
 
-x_train <- as.matrix(select(train, -DEATH_EVENT))
-x_test <- as.matrix(select(test, -DEATH_EVENT))
-y_train <- as.matrix(train$DEATH_EVENT)
+x_train <- select(train, -DEATH_EVENT)
+x_test <- select(test, -DEATH_EVENT)
+y_train <- train$DEATH_EVENT
 y_test <- test$DEATH_EVENT 
 
-logistic_model2 <- glm(DEATH_EVENT ~ ., data = train, family = "binomial")
-summary(logistic_model2)
+set.seed(123)
 
-logistic_model <- glmnet(x_train, y_train, family = "binomial", alpha = 0)
-plot(logistic_model, xvar = "lambda", label = TRUE)
+# Resampled data, time removed, transformations applied
+logistic_model <- glm(DEATH_EVENT ~ ., data = train, family = "binomial")
+summary(logistic_model)
 
-cv_glm <- cv.glmnet(x_train, y_train, family = "binomial", k = 5)
+predictions <- predict(logistic_model, newdata = x_test, type = "response") %>%
+    as.data.frame() %>%
+    `colnames<-`(c("death_prob")) %>%
+    mutate(pred = as.numeric(death_prob > 0.5)) %>%
+    cbind(., y_test)
 
-predictions <- predict(logistic_model, newx = x_test, type = "response", 
-                       s = cv_glm$lambda.min) %>%
+x_train_reduced <- as.matrix(select(x_train, -diabetes))
+x_test_reduced <- as.matrix(select(x_test, -diabetes))
+
+set.seed(123)
+
+# K-fold CV lambda, alpha controlled
+logistic_model_2 <- glmnet(x_train_reduced, y_train, family = "binomial", 
+                           alpha = 0)
+
+cv_glm <- cv.glmnet(x_train_reduced, y_train, family = "binomial", k = 10)
+
+predictions_2 <- predict(logistic_model_2, newx = x_test_reduced, 
+                         type = "response", s = cv_glm$lambda.min) %>%
     as.data.frame() %>%
     `colnames<-`(c("death_prob")) %>%
     mutate(pred = as.numeric(death_prob > 0.5)) %>%
@@ -51,30 +93,15 @@ ct
 accuracy <- sum(diag(ct)) / sum(ct)
 accuracy
 
-# pred_data <- cbind(select(boot_data, -DEATH_EVENT), predictions)
-
-x_train_reduced <- as.matrix(select(as.data.frame(x_train), -anaemia, -diabetes, 
-                                    -high_blood_pressure, -smoking, -time))
-x_test_reduced <- as.matrix(select(as.data.frame(x_test), -anaemia, -diabetes, 
-                                   -high_blood_pressure, -smoking, -time))
-
-new_train <- select(as.data.frame(train), -anaemia, -diabetes,
-                    -high_blood_pressure, -smoking)
-
-logistic_model4 <- glm(DEATH_EVENT ~ ., data = new_train, family = "binomial")
-summary(logistic_model4)
-
-logistic_model3 <- glmnet(x_train_reduced, y_train, family = "binomial", 
-                         alpha = 0)
-
-predictions <- predict(logistic_model3, newx = x_test_reduced, 
-                       type = "response", s = 0.01) %>%
-    as.data.frame() %>%
-    `colnames<-`(c("death_prob")) %>%
-    mutate(pred = as.numeric(death_prob > 0.5)) %>%
-    cbind(., y_test)
-
-ct_reduced <- table(predictions$pred, predictions$y_test)
+ct_reduced <- table(predictions_2$pred, predictions_2$y_test)
 ct_reduced
 
 sum(diag(ct_reduced)) / sum(ct_reduced)
+
+# Precision
+ct[2, 2] / (ct[1, 2] + ct[2, 2])
+ct_reduced[2, 2] / (ct_reduced[1, 2] + ct_reduced[2, 2])
+
+# Recall
+ct[2, 2] / (ct[2, 1] + ct[2, 2])
+ct_reduced[2, 2] / (ct_reduced[2, 1] + ct_reduced[2, 2])
